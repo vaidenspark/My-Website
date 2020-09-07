42ef6525cd23fc41a54d85339ce453996a664742
rexdavid37 <kingedavid1995@gmail.com>
2020-09-07 14:31:47 -0500

    Completed all calculations

--- My-Website/src/app/shared/calc-serv.service.ts
+++ My-Website/src/app/shared/calc-serv.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { create, all } from 'mathjs';
 
 
+const math = create(all, { });
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,6 @@ import { catchError, retry } from 'rxjs/operators';
 export class CalcServService {
 
   constructor() { }
-
    // tslint:disable-next-line: typedef
    toDegrees(angle) {
     return angle * (180 / Math.PI);
@@ -20,15 +21,34 @@ export class CalcServService {
     return angle * (Math.PI / 180);
   }
   // tslint:disable-next-line: typedef
-  findA2(a1: number, r1: number, r2: number){
-    const numerator = r1 * Math.sin(this.toRadians(a1));
+  findA2(a1: number, r1: number, r2: number): number{
+    const numerator = r1 * (math.sin(this.toRadians(a1)));
     const denom = r2;
-    return this.toDegrees(Math.asin(numerator / denom));
+
+    return math.round((this.toDegrees(math.asin((numerator / denom)))), 3);
   }
   // tslint:disable-next-line: typedef
-  findA1(r1: number, a2: number, r2: number){
+  findA1(r1: number, a2: number, r2: number): number{
     const numerator = (Math.sin(this.toRadians(a2)) * r2);
     const denom = r1;
-    return this.toDegrees(Math.asin(numerator / denom));
+    return math.round((this.toDegrees(math.evaluate(`asin((${r2}*(sin(${this.toRadians(a2)})))/${r1})`))), 3);
+  }
+
+  findR1(a1: number, a2: number, r2: number): number{
+    a1 = this.toRadians(a1);
+    a2 =  this.toRadians(a2);
+    const numerator = math.sin(a2) * r2;
+    const denom = math.sin(a1);
+    return math.round((numerator / denom), 3);
+  }
+
+  findR2(a1: number, r1: number, a2: number): number{
+    a1 = this.toRadians(a1);
+    a2 =  this.toRadians(a2);
+
+
+    const numerator = math.sin(a1) * r1;
+    const denom = math.sin(a2);
+    return math.round((numerator / denom), 3);
   }
 }
--- My-Website/src/app/snell-calc/snell-calc.component.ts
+++ My-Website/src/app/snell-calc/snell-calc.component.ts
@@ -1,51 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import { CalcServService } from '../shared/calc-serv.service';
+import * as math from 'mathjs';
 
 
 @Component({
   selector: 'app-snell-calc',
   templateUrl: './snell-calc.component.html',
-  styleUrls: ['./snell-calc.component.scss']
+  styleUrls: ['./snell-calc.component.scss'],
+  template: `
+    <h3>{{validator}}</h3>
+    `
 })
 export class SnellCalcComponent implements OnInit {
+  validator = '';
 
   constructor(private calcServ: CalcServService) { }
   calcForm = new FormGroup({
-  a1: new FormControl(''),
-  r1: new FormControl(''),
-  a2: new FormControl(''),
-  r2: new FormControl('')
+    a1: new FormControl(''),
+    r1: new FormControl(''),
+    a2: new FormControl(''),
+    r2: new FormControl('')
   });
 
-  onSubmit(){
-
-    let a1 = this.calcForm.get('a1').value;
-    let r1 = this.calcForm.get('r1').value;
-    let a2 = this.calcForm.get('a2').value;
-    let r2 = this.calcForm.get('r1').value;
-
-    const isCalcA1 = (a1 === '') && (r1 !== '') && (a2 !== '') &&
-    (r2 !== '');
-    
-    const isCalcA2 = (a1 !== '') && (r1 !== '') && (a2 === '') &&
-    (r2 !== '');
-    if (isCalcA1){
-      this.calcForm.get('a1').setValue(this.calcServ.findA1(r1, a2, r2));
-      console.log(this.calcServ.findA1(r1, a2, r2));
-
-    }
-    if (isCalcA2){
-      this.calcForm.get('a2').setValue(this.calcServ.findA2(a1, r1, r2));
-      console.log(this.calcServ.findA2(a1, r1, r2));
-
-    }
-    else{
-      console.log("invalid input");
-    }
-  }
-
   ngOnInit(): void {
-  }
 
+  }
+  onSubmit(): void{
+        const a1 = this.calcForm.get('a1').value;
+        const r1 = this.calcForm.get('r1').value;
+        const a2 = this.calcForm.get('a2').value;
+        const r2 = this.calcForm.get('r2').value;
+
+
+        const isCalcA1 = (a1 === '') && (r1 !== '') && (a2 !== '') &&
+    (r2 !== '');
+
+        const isCalcA2 = (a1 !== '') && (r1 !== '') && (a2 === '') &&
+    (r2 !== '');
+
+        const isCalcR1 = (a1 !== '') && (r1 === '') && (a2 !== '') &&
+    (r2 !== '');
+
+        const isCalcR2 = (a1 !== '') && (r1 !== '') && (a2 !== '') &&
+    (r2 === '');
+
+        const isValidate = (a1 !== '') && (r1 !== '') && (a2 !== '') &&
+    (r2 !== '');
+
+        const allEmpty = (a1 === '') && (r1 === '') && (a2 === '') &&
+        (r2 === '');
+
+        if (isCalcA1){
+      this.calcForm.get('a1').setValue(this.calcServ.findA1(r1, a2, r2));
+    }
+        if (isCalcA2){
+      this.calcForm.get('a2').setValue(this.calcServ.findA2(a1, r1, r2));
+    }
+        if (isCalcR1){
+      this.calcForm.get('r1').setValue(this.calcServ.findR1(a1, a2, r2));
+  }
+        if (isCalcR2){
+      this.calcForm.get('r2').setValue(this.calcServ.findR2(a1, r1, a2));
+}
+        if (isValidate){
+      const test = this.calcServ.findA2(a1, r1, r2);
+      if (test !== this.calcForm.get('a2').value){
+        this.validator = 'Invalid Inputs';
+      }
+      if (math.round(test) === math.round(this.calcForm.get('a2').value)){
+        this.validator = 'Correct!';
+      }
+
+    }
+        if (allEmpty){
+      this.validator = 'Please enter Some values';
+    }
+
+  }
 }
